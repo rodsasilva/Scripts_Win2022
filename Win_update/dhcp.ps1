e4785e15c6148bed7e310e94d8f9bb329d8b4fe7
Rodrigo Sabino <41909607+rodsasilva@users.noreply.github.com>
2024-09-23 17:21:42 -0300

    roberlan - perigoso

--- Win_update/dhcp.ps1
+++ Win_update/dhcp.ps1
@@ -3,12 +3,34 @@ $StartRange="172.16.100.130"
 $EndRange="172.16.100.254" 
 $SubnetMask="255.255.255.128" 
 $LeaseDuration="1.00:00:00" 
-$ScopeId="172.16.100.128" 
+$ScopeId="172.16.100.128" # $(StartRange)-2
 $ComputerName="srvcstau0104.sfirjan.sf" 
 $DnsServer="172.16.96.54", "10.255.15.13" 
 $DnsDomain="sfirjan.sf" 
-$Router="172.16.100.129" 
+$Router="172.16.100.129" # $(StartRange)-1
 
+###
+$Name="SSNBEN-VOIP" 
+$StartRange="172.16.101.2" 
+$EndRange="172.16.101.253" 
+$SubnetMask="255.255.255.0" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.101.0" # $(StartRange)-2
+$ComputerName="srvcstau0104.sfirjan.sf" 
+$DnsServer="172.16.96.54", "10.255.15.13" 
+$DnsDomain="sfirjan.sf" 
+$Router="172.16.101.1" # $(StartRange)-1
+###
+$Name="SSNBEN-ACCESS-POINT" 
+$StartRange="172.16.105.130" 
+$EndRange="172.16.105.254" 
+$SubnetMask="255.255.255.128" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.105.128" # $(StartRange)-2
+$ComputerName="srvcstau0104.sfirjan.sf" 
+$DnsServer="172.16.96.54", "10.255.15.13" 
+$DnsDomain="sfirjan.sf" 
+$Router="172.16.105.129" # $(StartRange)-1
 ###
 
 
