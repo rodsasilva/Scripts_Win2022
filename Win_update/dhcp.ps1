007a64623ccaa1a1494fbd15858f503dd4342edc
Rodrigo Sabino <41909607+rodsasilva@users.noreply.github.com>
2024-09-23 17:55:10 -0300

    DHCP EDUCA SSNBEN

--- Win_update/dhcp.ps1
+++ Win_update/dhcp.ps1
@@ -1,38 +1,167 @@
-$Name="SSNBEN-SEG.PATRIMONIAL" 
-$StartRange="172.16.100.130" 
-$EndRange="172.16.100.254" 
-$SubnetMask="255.255.255.128" 
-$LeaseDuration="1.00:00:00" 
-$ScopeId="172.16.100.128" # $(StartRange)-2
-$ComputerName="srvcstau0104.sfirjan.sf" 
-$DnsServer="172.16.96.54", "10.255.15.13" 
-$DnsDomain="sfirjan.sf" 
-$Router="172.16.100.129" # $(StartRange)-1
-
-###
-$Name="SSNBEN-VOIP" 
-$StartRange="172.16.101.2" 
-$EndRange="172.16.101.253" 
+$Name="SSNBEN-SSN EDUCA" 
+$StartRange="172.16.54.2" 
+$EndRange="172.16.54.254" 
 $SubnetMask="255.255.255.0" 
 $LeaseDuration="1.00:00:00" 
-$ScopeId="172.16.101.0" # $(StartRange)-2
-$ComputerName="srvcstau0104.sfirjan.sf" 
-$DnsServer="172.16.96.54", "10.255.15.13" 
-$DnsDomain="sfirjan.sf" 
-$Router="172.16.101.1" # $(StartRange)-1
+$ScopeId="172.16.54.0" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.54.1" # $(StartRange)-1
 ###
-$Name="SSNBEN-ACCESS-POINT" 
-$StartRange="172.16.105.130" 
-$EndRange="172.16.105.254" 
-$SubnetMask="255.255.255.128" 
+$Name="SSNBEN-BIBLIOTECA" 
+$StartRange="172.16.111.194" 
+$EndRange="172.16.54.254" 
+$SubnetMask="255.255.255.192" 
 $LeaseDuration="1.00:00:00" 
-$ScopeId="172.16.105.128" # $(StartRange)-2
-$ComputerName="srvcstau0104.sfirjan.sf" 
-$DnsServer="172.16.96.54", "10.255.15.13" 
-$DnsDomain="sfirjan.sf" 
-$Router="172.16.105.129" # $(StartRange)-1
+$ScopeId="172.16.111.192" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.111.193" # $(StartRange)-1
 ###
-
+$Name="SSNBEN-SFRJ EVENTOS" 
+$StartRange="172.16.103.2" 
+$EndRange="172.16.103.254" 
+$SubnetMask="255.255.255.0" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.103.0" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.103.1" # $(StartRange)-1
+###
+$Name="SSNBEN-SALA-CONECTADA" 
+$StartRange="172.16.111.130" 
+$EndRange="172.16.111.190" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.111.128" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.111.129" # $(StartRange)-1
+###
+$Name="SSNBEN-AD-EDUCACAO" 
+$StartRange="172.16.107.2" 
+$EndRange="172.16.107.62" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.107.0" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.107.1" # $(StartRange)-1
+###
+$Name="SSNBEN-LAB-311"
+$StartRange="172.16.111.66" 
+$EndRange="172.16.111.126" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.111.64" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.111.65" # $(StartRange)-1
+###
+$Name="SSNBEN-LAB-312"
+$StartRange="172.16.111.2" 
+$EndRange="172.16.111.62" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.111.0" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.111.1" # $(StartRange)-1
+###
+$Name="SSNBEN-PREACELERA"
+$StartRange="172.16.110.194" 
+$EndRange="172.16.110.254" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.110.192" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.110.193" # $(StartRange)-1
+###
+$Name="SSNBEN-FABLAB"
+$StartRange="172.16.110.130" 
+$EndRange="172.16.110.190" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.110.128" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.110.129" # $(StartRange)-1
+###
+$Name="SSNBEN-AUTOMACAO-SL313"
+$StartRange="172.16.110.66" 
+$EndRange="172.16.110.126" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.110.64" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.110.65" # $(StartRange)-1
+###
+$Name="SSNBEN-AUTOMACAO-SL314"
+$StartRange="172.16.110.2" 
+$EndRange="172.16.110.62" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.110.0" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.110.1" # $(StartRange)-1
+###
+$Name="SSNBEN-LAB-REDES INDUS.-SL317"
+$StartRange="172.16.109.194" 
+$EndRange="172.16.109.254" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.109.192" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.109.193" # $(StartRange)-1
+###
+$Name="SSNBEN-LAB-ELETRONICA-SL301"
+$StartRange="172.16.109.130" 
+$EndRange="172.16.109.190" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.109.128" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.109.129" # $(StartRange)-1
+###
+$Name="SSNBEN-LAB-INSTRUMENTACAO-SL301"
+$StartRange="172.16.109.66" 
+$EndRange="172.16.109.126" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.109.64" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.109.65" # $(StartRange)-1
+###
+$Name="SSNBEN-SERVICOS TECNOLOGICOS"
+$StartRange="172.16.108.194"
+$EndRange="172.16.108.254" 
+$SubnetMask="255.255.255.192" 
+$LeaseDuration="1.00:00:00" 
+$ScopeId="172.16.109.192" # $(StartRange)-2
+$ComputerName="srveductsaut01.sfirjan.sf" 
+$DnsServer="172.16.107.2", "10.255.15.15" , "10.255.15.16" 
+$DnsDomain="educa.sf" 
+$Router="172.16.109.193" # $(StartRange)-1
 
 Add-DhcpServerv4Scope -Name $Name -StartRange $StartRange -EndRange $EndRange -SubnetMask  $SubnetMask -LeaseDuration $LeaseDuration -State Active
 Set-DhcpServerv4OptionValue -ScopeId $ScopeId -ComputerName $ComputerName -DnsServer $DnsServer -DnsDomain $DnsDomain -Router $Router -Force
